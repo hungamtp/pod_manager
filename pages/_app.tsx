0627c1861873e971554f0b5e0d3f8b1c8cd11ae1
duynk-hub <duyuminh41@gmail.com>
2022-05-26 14:42:33 +0700

    add login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import { AppPropsWithLayout } from "@/models/common";
 import { EmptyLayout } from "@/components/layouts";
+import "../styles/globals.css";
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout;
   return (
