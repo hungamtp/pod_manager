867ad4124387146fe2ddffd44d010519eb88c9ca
studingHtml <hieuthomnghiep@gmail.com>
2022-07-03 16:27:33 +0700

    fix pubish error

--- hooks/products/use-publish-product.ts
+++ hooks/products/use-publish-product.ts
@@ -1,41 +1,39 @@
-import { ErrorHttpResponse } from '@/models/error_http_response.interface';
-import ManageProduct from '@/pages/manage-product';
-import { publishProduct } from '@/services/products';
-import { AxiosError } from 'axios';
-import { useRouter } from 'next/router';
-import { useSnackbar } from 'notistack';
-import { useMutation, useQueryClient } from 'react-query';
+import { ErrorHttpResponse } from "@/models/error_http_response.interface";
+import ManageProduct from "@/pages/manage-product";
+import { publishProduct } from "@/services/products";
+import { AxiosError } from "axios";
+import { useRouter } from "next/router";
+import { useSnackbar } from "notistack";
+import { useMutation, useQueryClient } from "react-query";
 
-
-const usePublishProduct = (handleClosePublishDialog:() => void) => {
-	const router = useRouter();
-    const queryClient = useQueryClient();
-	const { enqueueSnackbar } = useSnackbar();
-	return useMutation(
-		      
-        async (id: string) => {
-            return await publishProduct(id);
-		},
-		{
-			onSettled: (data) => {
-				handleClosePublishDialog()
-                queryClient.invalidateQueries("Products")
-				enqueueSnackbar("Create successfully!", {
-					autoHideDuration: 3000,
-					variant: "success",
-				  });
-			},
-			onError: (error: AxiosError<ErrorHttpResponse>) => {
-				if (error) {
-					enqueueSnackbar(error.response?.data.errorMessage, {
-					  autoHideDuration: 9000,
-					  variant: "error",
-					});
-				  }
-			},
-			
-		}
-	);
+const usePublishProduct = (handleClosePublishDialog: () => void) => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const { enqueueSnackbar } = useSnackbar();
+  return useMutation(
+    async (id: string) => {
+      return await publishProduct(id);
+    },
+    {
+      onSuccess: (data) => {
+        handleClosePublishDialog();
+        queryClient.invalidateQueries("Products");
+        enqueueSnackbar("Pubish successfully!", {
+          autoHideDuration: 3000,
+          variant: "success",
+        });
+      },
+      onError: (error: AxiosError<ErrorHttpResponse>) => {
+        if (error) {
+          handleClosePublishDialog();
+          enqueueSnackbar(error.response?.data.errorMessage, {
+            autoHideDuration: 9000,
+            variant: "error",
+          });
+        }
+      },
+    }
+  );
 };
 
 export default usePublishProduct;
