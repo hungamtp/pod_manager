879bfbd3a050bb194dcc5f3653544c269ee71d23
studingHtml <hieuthomnghiep@gmail.com>
2022-08-26 23:27:53 +0700

    update date ui

--- helpers/date-utils.ts
+++ helpers/date-utils.ts
@@ -1,5 +1,5 @@
 import moment from "moment";
 export const dateFormat = (inputDate: Date | string) => {
   //parse the input date
-  return moment(inputDate).format("HH:mm | DD-MM-YYYY");
+  return moment(inputDate).format("HH:mm DD-MM-YYYY");
 };
